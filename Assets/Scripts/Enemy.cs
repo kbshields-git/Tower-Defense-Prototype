42d7affe513c78486bac62a6c809892fd3bf264b
hmmrfll <bigblack@gmail.com>
2018-05-30 02:38:48 -0400

    Initial Commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] NavMeshAgent agent;
+    bool TargetSet = false;
+    GameObject targetExit;
+
+    private void Awake()
+    {
+        // Search the scene for the object labeled Exit
+        targetExit = GameObject.FindGameObjectWithTag("Exit");
+        
+    }
+    // Update is called once per frame
+    void Update () {
+        if (!TargetSet) {
+            agent.SetDestination(targetExit.gameObject.transform.position);
+            if (agent.hasPath)
+            {
+                TargetSet = true;
+            }            
+        }
+	}
+}
