c1abbca62b320d523f766c1898f37e109abbe985
Brock Oli <bigblack@gmail.com>
2018-05-31 00:42:57 -0400

    Turret Started

---
+++ Assets/Turret.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+    public Transform target;
+    public float range = 15f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
