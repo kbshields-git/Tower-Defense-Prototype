81e8dca091973ab585dc77520ae6473d78d0db0d
Brock Oli <bigblack@gmail.com>
2018-06-07 01:26:56 -0400

    Added 'glow' to bullets
    Bullets now glow and have rolloff on the glow, like a bullet cooling down... at least a cheap representation of it.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Bullet : MonoBehaviour {
 
@@ -9,13 +10,32 @@ public class Bullet : MonoBehaviour {
     public float m_Velocity = 10f;
     public int m_BaseDamage = 1;
 
-    public void AcquireTarget(Enemy _target)
+    public bool m_IsLit;
+    [MinMaxRange(0,2)]
+    public RangedFloat m_RandLightFalloffRange;
+    private float m_RandLightFalloff;
+    private float m_RemainingTime;
+    public Light m_BulletGlow;
+
+    /// <summary>
+    /// Holds the starting intesnity of a bullets light, if it has one.
+    /// </summary>
+    private float m_Intensity;
+
+ 
+	void Awake()
     {
-        target = _target;
+        if (m_IsLit)
+        {
+            m_Intensity = m_BulletGlow.intensity;
+            m_RandLightFalloff = Random.Range(m_RandLightFalloffRange.minValue, m_RandLightFalloffRange.maxValue);
+            m_RemainingTime = m_RandLightFalloff;
+        }
     }
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 
         if (target == null)
         {
@@ -24,6 +44,24 @@ public class Bullet : MonoBehaviour {
             return;
         }
 
+        if (m_IsLit)
+        {
+            RolloffGlow();
+        }
+
+        Fly();
+    }
+
+    private void RolloffGlow()
+    {       
+        m_RemainingTime -= Time.deltaTime;
+        Debug.Log(m_RemainingTime / m_RandLightFalloff);
+        m_BulletGlow.intensity = m_Intensity * (m_RemainingTime / m_RandLightFalloff);
+        
+    }
+
+    private void Fly()
+    {
         Vector3 dir = target.gameObject.transform.position - transform.position;
         float distanceThisFrame = m_Velocity * Time.deltaTime;
 
@@ -33,8 +71,13 @@ public class Bullet : MonoBehaviour {
             return;
         }
 
-        transform.Translate(dir.normalized * distanceThisFrame, Space.World);		
-	}
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    public void AcquireTarget(Enemy _target)
+    {
+        target = _target;
+    }
 
     void HitTarget()
     {
