0f9ed44d394aaa470af4a318f7f951f1c46122c1
Brock Oli <bigblack@gmail.com>
2018-06-29 01:17:06 -0400

    Episode 2 of ProcGen Down

--- Assets/Scripts/WorldGen/MapGenerator.cs
+++ Assets/Scripts/WorldGen/MapGenerator.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGenerator : MonoBehaviour {
+public class MapGenerator : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public bool autoUpdate;
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+
+    }
+
 }
