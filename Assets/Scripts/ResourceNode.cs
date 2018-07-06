56fae4bb6626d405404783c5c7f12bef6f4bc1f6
Brock Oli <bigblack@gmail.com>
2018-07-06 01:17:04 -0400

    Glow effect on MouseOver of Resource Node

--- Assets/Scripts/ResourceNode.cs
+++ Assets/Scripts/ResourceNode.cs
@@ -9,29 +9,41 @@ public class ResourceNode : MonoBehaviour
     public enum ResourceType { Crystal, Gold, Iron };
     public ResourceType nodeType;
     [SerializeField] Light hoverLight;
-    float initialLightIntensity;
+    public float initialLightIntensity;
+    public float glowSpeed;
     float hoverLightIntensity;
 
     [MinMaxRange(100, 10000)]
     public RangedFloat startingResources;
     float currentResources;
 
+    bool mouseOverNode;
+
     void Start()
     {
         currentResources = Random.Range(startingResources.minValue, startingResources.maxValue);
         initialLightIntensity = hoverLight.intensity;
         hoverLightIntensity = initialLightIntensity * 1.5f;
+        mouseOverNode = false;
     }
 
+    void Update()
+    {
+        if (!mouseOverNode && hoverLight.intensity > initialLightIntensity)
+        {
+			hoverLight.intensity = Mathf.Lerp(hoverLight.intensity, initialLightIntensity, Time.deltaTime * glowSpeed);
+        }
+    }
     void OnMouseOver()
     {
-        print("Mouse over Resource Node");
-        hoverLight.intensity = hoverLightIntensity;
+        mouseOverNode = true;
+        hoverLight.intensity = Mathf.Lerp(hoverLight.intensity, hoverLightIntensity, Time.deltaTime * glowSpeed);
     }
 
     void OnMouseExit()
     {
-        hoverLight.intensity = initialLightIntensity;
+
+        mouseOverNode = false;
     }
 
 }
