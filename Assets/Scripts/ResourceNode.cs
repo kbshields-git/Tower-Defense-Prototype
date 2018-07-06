364ad6bb92a251a65b87bd31b62932cf30fdaa3a
Brock Oli <bigblack@gmail.com>
2018-07-06 00:46:56 -0400

    Resource Node Glows on MouseOver

---
+++ Assets/Scripts/ResourceNode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ResourceNode : MonoBehaviour
+{
+
+    public enum ResourceType { Crystal, Gold, Iron };
+    public ResourceType nodeType;
+    [SerializeField] Light hoverLight;
+    float initialLightIntensity;
+    float hoverLightIntensity;
+
+    [MinMaxRange(100, 10000)]
+    public RangedFloat startingResources;
+    float currentResources;
+
+    void Start()
+    {
+        currentResources = Random.Range(startingResources.minValue, startingResources.maxValue);
+        initialLightIntensity = hoverLight.intensity;
+        hoverLightIntensity = initialLightIntensity * 1.5f;
+    }
+
+    void OnMouseOver()
+    {
+        print("Mouse over Resource Node");
+        hoverLight.intensity = hoverLightIntensity;
+    }
+
+    void OnMouseExit()
+    {
+        hoverLight.intensity = initialLightIntensity;
+    }
+
+}
